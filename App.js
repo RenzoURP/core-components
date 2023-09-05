12392698ce94ed502e02d2dbec919587d217ca47
RenzoURP <137726724+RenzoURP@users.noreply.github.com>
2023-09-05 20:15:26 +0000

    05/09/23 Contacts App

--- App.js
+++ App.js
@@ -1,57 +1,114 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
-import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert, TextInput, SafeAreaView, VirtualizedList, FlatList } from 'react-native';
+import React, { Component } from 'react';
+
+
+
+
+
 
 export default function App() {
-  const [contact, setContact] = React.useState("");
 
-  //TODO: Tip 1 for the assignment 2
-  //const [contacts, setContacts] = React.useState([]);
+  const [contact, setContacto] = React.useState('');
+  const [lista_contactos, setListaContactos] = React.useState([]);
 
-  const addContactHandler = () => {
-    console.log(contact);
-
-    //TODO: Tip 2 for the assignment 2
-    //setContacts([...contacts, { name: contact }]);
-  };
-  return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Contact information"
-          onChangeText={(text) => setContact(text)}
-        />
-        <Button title="Add Contact" onPress={addContactHandler} />
-      </View>
-      <View style={styles.contactsContainer}>
-        <Text>List of Contacts...</Text>
-      </View>
+  //* Renderiza cada item de la lista
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.nombre}</Text>
     </View>
+  );
+
+  //*Función para añadir contactos a la lista de contactos
+  const añadir_contacto = () => {
+    //* Verifica si se ingresó un contacto y lo agrega a la lista
+    if (contact) {
+      const newContact = { nombre: contact };
+      setListaContactos([...lista_contactos, newContact]);
+      setContacto("");
+    }
+  };
+  
+
+
+  return (
+    //*View de la app
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.input_container}>
+        <Text>Contacto</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Ingresar Contacto'
+          onChangeText={(contacto) => setContacto(contacto)}
+          value={contact}
+        />
+        <Button
+          title="Agregar Contacto"
+          onPress={añadir_contacto}
+        />
+
+      </View>
+      <View style= {styles.contacts_container}>
+        <Text> Lista de Contactos</Text>
+
+
+        <FlatList
+          data={lista_contactos}
+          renderItem={renderItem}
+          keyExtractor={(contacto, index) => index.toString()}
+        />
+      </View>
+
+
+
+
+      <StatusBar style="auto" />
+
+    </SafeAreaView>
   );
 }
 
+
+//* Estilos para la app
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    padding: 80,
-    paddingHorizontal: 16,
+  text_contacto:{
+    paddingBottom: 30
   },
-  inputContainer: {
+  input: {
+    height: 40,
+    width: 190,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+    
+  },
+  button: {
+    height: 30,
+    color: "#fff",
+  },
+  input_container: {
     flex: 1,
     flexDirection: "column",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    width: '100%',
+    backgroundColor: "#8CAE68",
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop: 50,
+    padding: 0
   },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 10,
-    padding: 10,
+  contacts_container: {
+    flex: 2,
+    marginTop: 20,
+    width: '100%',
+    justifyContent: "center",
+    alignItems: 'center',
+    marginBottom: 20
   },
-  contactsContainer: {
-    flex: 6,
+  container: {
+    flex: 1,
+    backgroundColor: '#DFE0E2',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
